768d13d33ec5efad3df04e99d44bd127d414e727
Andres Montano <amontano@virginia.edu>
2013-06-19 18:45:58 -0600

    moving default perspective to kmaps engine

--- lib/subjects_engine/session_manager.rb
+++ lib/subjects_engine/session_manager.rb
@@ -1,25 +1,14 @@
 module SubjectsEngine
   module SessionManager
     protected
-    def current_perspective
-      perspective_id = session[:perspective_id]
-      begin
-        if perspective_id.blank?
-          @@current_perspective = Perspective.get_by_code('hum') if !defined?(@@current_perspect) || @@current_perspective.nil? || @@current_perspective.code != 'pol.admin.hier'
-        else
-          @@current_perspective = Perspective.find(perspective_id) if !defined?(@@current_perspect) || @@current_perspective.nil? || @@current_perspective.id != perspective_id
-        end
-      rescue ActiveRecord::RecordNotFound
-        session[:perspective_id] = nil
-        @@current_perspective = Perspective.get_by_code('hum')
-      end
-      return @@current_perspective
+    def default_perspective_code
+      'hum'
     end
     
     # Inclusion hook to make #current_perspective
     # available as ActionView helper method.
     def self.included(base)
-      base.send :helper_method, :current_perspective
+      base.send :helper_method, :default_perspective_code
     end
   end
 end
