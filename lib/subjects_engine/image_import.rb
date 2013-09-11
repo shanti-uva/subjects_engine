0d69e17a0f29503a5a9695550a772e3162243359
Andres Montano <amontano@virginia.edu>
2013-09-11 00:11:11 -0600

    adding logging

--- lib/subjects_engine/image_import.rb
+++ lib/subjects_engine/image_import.rb
@@ -1,10 +1,13 @@
 class ImageImport
   def self.do_image_import
     Feature.order(:fid).each do |f|
+      puts "Checking feature #{f.fid}..."
       next if !f.illustration.nil?
       topic = MmsIntegration::Topic.find(f.fid) #MAKE TASK SPECIFIC TO SUBJECTS AND PLACES
+      next if topic.nil?
       pic = topic.pictures.first
       next if pic.nil?
+      puts "Adding picture #{pic.id}."
       f.create_illustration(:picture_id => pic.id, :picture_type => 'MmsIntegration::Picture', :is_primary => true)
     end
   end
