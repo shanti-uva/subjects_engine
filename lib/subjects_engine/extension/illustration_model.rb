d914b117c79e8fba32da48d1f9896c20b893da56
Andres Montano <amontano@virginia.edu>
2013-10-12 20:35:31 -0600

    updating calculation of country for display of location in illustrations

--- lib/subjects_engine/extension/illustration_model.rb
+++ lib/subjects_engine/extension/illustration_model.rb
@@ -1,14 +1,28 @@
 module SubjectsEngine
   module Extension
     module IllustrationModel
+      include ActionView::Helpers
       extend ActiveSupport::Concern
       
       included do
       end
       
-      def location
+      def place
         fid = self.picture.location
         fid.nil? ? nil : PlacesIntegration::Feature.find(fid)
+      end
+      
+      def location
+        pl = self.place
+        return nil if pl.nil?
+        perspectives = pl.perspectives
+        per = perspectives.detect{|p| p.code=='pol.admin.hier'}
+        ancestors = per.nil? ? perspectives.collect(&:ancestors).flatten.size : per.ancestors
+        parent = ancestors.detect{|a| a.id != pl.id && a.feature_types.detect{|ft| ft.id=='29'}}
+        parent = pl.parents.first if parent.nil?
+      	str = pl.header
+      	str << ", #{parent.header}" if !parent.nil?
+        return link_to str, PlacesIntegration::Feature.element_url(pl.id, :format => 'html')
       end
       
       module ClassMethods
