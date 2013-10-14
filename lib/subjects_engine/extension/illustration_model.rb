5f52acca09da368ac2afaeea0fa50c3dbeae345b
Andres Montano <amontano@virginia.edu>
2013-10-14 15:06:20 -0600

    making access to mms api more standard

--- lib/subjects_engine/extension/illustration_model.rb
+++ lib/subjects_engine/extension/illustration_model.rb
@@ -1,7 +1,6 @@
 module SubjectsEngine
   module Extension
     module IllustrationModel
-      include ActionView::Helpers
       extend ActiveSupport::Concern
       
       included do
@@ -10,19 +9,6 @@ module SubjectsEngine
       def place
         fid = self.picture.location
         fid.nil? ? nil : PlacesIntegration::Feature.find(fid)
-      end
-      
-      def location
-        pl = self.place
-        return nil if pl.nil?
-        perspectives = pl.perspectives
-        per = perspectives.detect{|p| p.code=='pol.admin.hier'}
-        ancestors = per.nil? ? perspectives.collect(&:ancestors).flatten.size : per.ancestors
-        parent = ancestors.detect{|a| a.id != pl.id && a.feature_types.detect{|ft| ft.id=='29'}}
-        parent = pl.parents.first if parent.nil?
-      	str = pl.header
-      	str << ", #{parent.header}" if !parent.nil?
-        return link_to str, PlacesIntegration::Feature.element_url(pl.id, :format => 'html')
       end
       
       module ClassMethods
