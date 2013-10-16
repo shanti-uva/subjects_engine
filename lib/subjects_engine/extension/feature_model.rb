34cce2c14240eb6c16dc425deea4eef207a7610a
Andres Montano <amontano@virginia.edu>
2013-10-16 17:39:20 -0600

    fixing access to media api

--- lib/subjects_engine/extension/feature_model.rb
+++ lib/subjects_engine/extension/feature_model.rb
@@ -11,7 +11,7 @@ module SubjectsEngine
       end
 
       def kmap_path(type = nil)
-        a = ['places', self.fid]
+        a = ['topics', self.fid]
         a << type if !type.nil?
         a.join('/')
       end
@@ -24,6 +24,17 @@ module SubjectsEngine
         Rails.cache.fetch("#{self.cache_key}/feature_count", :expires_in => 1.day) do
           PlacesIntegration::FeatureCategoryCount.find(:all, :params => {:category_id => self.fid}).first.count
         end
+      end
+      
+      def media_count(options = {})
+        media_count_hash = Rails.cache.fetch("#{self.cache_key}/media_count", :expires_in => 1.day) do
+          media_place_count = MmsIntegration::MediaCategoryCount.find(:all, :params => {:category_id => self.fid}).dup
+          media_count_hash = { 'Medium' => media_place_count.shift.count.to_i }
+          media_place_count.each{|count| media_count_hash[count.medium_type] = count.count.to_i }
+          media_count_hash
+        end
+        type = options[:type]
+        return type.nil? ? media_count_hash['Medium'] : media_count_hash[type]
       end
 
       module ClassMethods
