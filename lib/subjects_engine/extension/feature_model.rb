fce009e643e8852a53c6cc3f0421159c4d24220f
Andres Montano <amontano@virginia.edu>
2014-01-17 18:14:20 -0600

    adding message for subject relation type

--- lib/subjects_engine/extension/feature_model.rb
+++ lib/subjects_engine/extension/feature_model.rb
@@ -21,13 +21,13 @@ module SubjectsEngine
       end
       
       def feature_count
-        Rails.cache.fetch("#{self.cache_key}/feature_count", :expires_in => 1.day) do
+        Rails.cache.fetch("#{self.cache_key}/feature_count", :expires_in => 1.week) do
           PlacesIntegration::FeatureCategoryCount.find(:all, :params => {:category_id => self.fid}).first.count
         end
       end
       
       def media_count(options = {})
-        media_count_hash = Rails.cache.fetch("#{self.cache_key}/media_count", :expires_in => 1.day) do
+        media_count_hash = Rails.cache.fetch("#{self.cache_key}/media_count", :expires_in => 1.week) do
           media_place_count = MmsIntegration::MediaCategoryCount.find(:all, :params => {:category_id => self.fid}).dup
           media_count_hash = { 'Medium' => media_place_count.shift.count.to_i }
           media_place_count.each{|count| media_count_hash[count.medium_type] = count.count.to_i }
