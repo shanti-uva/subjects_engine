520897099988cbd485e1e018d5bdc18b2b66891d
Derik Ramirez <drc2r@virginia.edu>
2016-12-20 12:37:19 -0600

    Moved associated media controller to kmaps_engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  resources :associated_media, only: :show do
-    member do
-      get 'pictures'
-      get 'videos'
-      get 'documents'
-    end
-  end
   
   get 'features/:id/places', to: 'places#show', as: 'feature_places'
   get 'features/:id/list_with_places', to: 'features#list_with_places'
