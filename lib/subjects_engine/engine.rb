d137f669947149ad3e249c263f14802d8025d592
Derik Ramirez <drc2r@virginia.edu>
2018-04-19 18:47:53 +0100

    Improvement for the related partial count and code refactor
    Displaying a more detailed count that now shows the parents and children
    counts.
    
    Refactored the code to take the logic out of the view and move it to the
    JavaScript code.

--- lib/subjects_engine/engine.rb
+++ lib/subjects_engine/engine.rb
@@ -1,7 +1,7 @@
 module SubjectsEngine
   class Engine < ::Rails::Engine
     initializer :assets do |config|
-      Rails.application.config.assets.precompile.concat(['subjects_engine/related.css'])
+      Rails.application.config.assets.precompile.concat(['subjects_engine/related.css','subjects_engine/related-section-initializer.js'])
     end
 
     initializer :loader do |config|
