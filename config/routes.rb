321f067ca6d4e33f3aaacd396676cfa75e23aa7b
Andres Montano <amontano@virginia.edu>
2020-10-07 21:15:08 -0500

    fixing display of media associated to subject

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :media, only: 'show', path: 'media_objects'
   get 'features/:id/places', to: 'places#show', as: 'feature_places'
   get 'features/:id/list_with_places', to: 'features#list_with_places'
   get 'features/:id/all_with_places', to: 'features#all_with_places'
