08cb70318d895d26b3e5cc2aa3ca0aa5f6393a0b
Andres Montano <amontano@virginia.edu>
2025-10-16 12:53:19 -0600

    not using custom load hooks

--- lib/subjects_engine/engine.rb
+++ lib/subjects_engine/engine.rb
@@ -3,5 +3,25 @@ module SubjectsEngine
     initializer :assets do |config|
       Rails.application.config.assets.precompile.concat(['subjects_engine/related.css','subjects_engine/related-section-initializer.js'])
     end
+    
+    config.to_prepare do
+      # Extending / overriding kmaps_engine controllers
+      require_dependency 'features_controller'
+      require_dependency 'subjects_engine/feature_controller_extensions'
+      FeaturesController.include SubjectsEngine::FeatureControllerExtensions
+      
+      require_dependency 'sessions_controller'
+      require_dependency 'subjects_engine/sessions_controller_extensions'
+      SessionsController.include SubjectsEngine::SessionsControllerExtensions
+      
+      # Extending / overriding kmaps_engine models
+      require_dependency 'feature'
+      require_dependency 'subjects_engine/feature_extensions'
+      Feature.include SubjectsEngine::FeatureExtensions
+      
+      require_dependency 'illustration'
+      require_dependency 'subjects_engine/illustration_extensions'
+      Illustration.include SubjectsEngine::IllustrationExtensions
+    end
   end
 end
